6a68766962a231034a1e2dba1267a29ef0346099
tealupu <tealupu@gmail.com>
2026-03-11 16:10:43 +0200

    Git

---
+++ lab1.py
@@ -0,0 +1,41 @@
+print (type(1))
+print (type(10))
+print (type(-100))
+print (type(-1.4))
+print(type("hello"))
+print(type(True))
+print(type(False))
+print(type(3+1.5+2.1-9.6))
+my_list=[1,3,'1',True,1,1,3,2]
+print(type(my_list))
+my_list.count(1)
+print(my_list.count(1))
+#dictionary
+my_dict={'name':'Teodora', 'age':'21', 'Student':'True'}
+print(my_dict)
+print(my_dict['age'])
+print(type(my_dict))
+#tuple
+my_tuple=(1,2,3,4)
+print(type(my_tuple))
+#set
+my_set=set()
+my_set.add(3)
+my_set.add(100)
+my_set.add(12)
+my_set.add('Tea')
+print(my_set)
+my_set.remove(3)
+print(my_set)
+print(type(None))
+a=None
+print(type(a))
+#string
+name='Tea'
+print(name[0])
+print(name[:])
+print(name[1:])
+print(name[:1])
+
+print(1==1)
+print(1==3)
